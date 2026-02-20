febeb750894371f5f3fd8b97df28bf59049a0884
HahmDY <hahmdong@kaist.ac.kr>
2026-02-20 07:31:11 +0000

    add ppo upload code

--- rmood/rl/ppo/upload.sh
+++ rmood/rl/ppo/upload.sh
@@ -1,12 +1,12 @@
 dataset_name=alpacafarm
-reward_model_name=rm
+reward_model_name=mrm-sft-based
 
-for step in 40; do
+for step in 10 20 30 40 50 60 70 80 90 100; do
 cd ~
 python -m verl.model_merger merge \
     --backend megatron \
-    --local_dir /root/checkpoints/penaltyrm/qwen2.5-3b-${reward_model_name}-ppo/global_step_${step}/actor \
-    --target_dir PRM-qwen2.5-3b-${dataset_name}-${reward_model_name}-ppo-step-${step}
-cd ~/PRM-qwen2.5-3b-${dataset_name}-${reward_model_name}-ppo-step-${step}
-huggingface-cli upload Hahmdong/PRM-qwen2.5-3b-${dataset_name}-${reward_model_name}-ppo-step-${step} .
+    --local_dir /root/checkpoints/RMOOD_PPO/RMOOD-qwen3-4b-${dataset_name}-${reward_model_name}-ppo/global_step_${step}/actor \
+    --target_dir RMOOD-qwen3-4b-${dataset_name}-${reward_model_name}-ppo-step-${step}
+cd ~/RMOOD-qwen3-4b-${dataset_name}-${reward_model_name}-ppo-step-${step}
+huggingface-cli upload Hahmdong/RMOOD-qwen3-4b-${dataset_name}-${reward_model_name}-ppo-step-${step} .
 done
