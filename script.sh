6159dacbb222ada34cfcbe60a7bd71629cdf691a
HahmDY <hahmdong@kaist.ac.kr>
2026-02-21 06:59:02 +0000

    update ppo results

---
+++ script.sh
@@ -0,0 +1,4 @@
+bash /root/reward-modeling-develop/rmood/rl/ppo/test/sampling_mrm.sh
+bash /root/reward-modeling-develop/rmood/rl/ppo/test/sampling_rm.sh
+
+bash /root/reward-modeling-develop/rmood/rl/ppo/test/get_kl.sh
